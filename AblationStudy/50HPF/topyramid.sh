bf86595774b41ab76cced5ba3f10526148d05f98
Marc Aubreville <marc.aubreville@fau.de>
2020-10-14 10:27:41 +0200

    Fixed an error

--- AblationStudy/50HPF/topyramid.sh
+++ AblationStudy/50HPF/topyramid.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Convert all TIFF files to pyramidal TIFF so they can be read with openslide without memory limitations
-for f in *.tiff; do vips --xres=4000 tiffsave $f pyramid/$f.tif  --compression=jpeg --Q=90 --tile --tile-width=256 --tile-height=256 --pyramid; done
+for f in *.tiff; do vips --xres=4000 tiffsave $f pyramid/$f  --compression=jpeg --Q=90 --tile --tile-width=256 --tile-height=256 --pyramid; done
 
